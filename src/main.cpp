432e2045519be5b9f020f6b9567539e1781c512d
Thomas Aschemann <taschemann@gmail.com>
2019-01-04 21:09:27 -0600

    Implemented skeleton game

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,16 @@
 #include <iostream>
-#include "game.h"
+#include "../include/Game.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    Game game;
+    bool gameInitResult = game.Initialize();
+
+    if (gameInitResult)
+    {
+        game.RunLoop();
+    }
+    game.Shutdown();
     return 0;
 }
