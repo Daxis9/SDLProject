179ec1647ce8a6ceb44b6c815a27c58e8a6cdf5f
Thomas Aschemann <taschemann@gmail.com>
2019-01-06 21:12:10 -0600

    Added includes for SDL Defined main the way SDL expects

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
+#include "SDL2/SDL.h"
 #include "../include/Game.h"
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
     Game game;
     bool gameInitResult = game.Initialize();
